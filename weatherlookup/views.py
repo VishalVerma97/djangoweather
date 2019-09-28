426ab57ee8b3fa8fc5899c00098198e4d8959bfc
Vishal Verma <v4vishalverma1997@gmail.com>
2019-09-28 17:07:24 +0530

    Added Hard Coded Api Call

--- weatherlookup/views.py
+++ weatherlookup/views.py
@@ -5,8 +5,18 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'home.html', {})
+    import json
+    import requests
+
+    api_request = requests.get("http://www.airnowapi.org/aq/forecast/zipCode/?format=application/json&zipCode=89129&date=2019-09-28&distance=25&API_KEY=B90E89A0-0AB7-4154-BBD6-1C8616A917C6")
+
+    try:
+        api = json.loads(api_request.content)
+    except Exception as e:
+        api = "Error ....."
+
+    return render(request, 'home.html', {'api': api})
 
 
 def about(request):
-    return  render(request, 'about.html', {})
+    return render(request, 'about.html', {})
